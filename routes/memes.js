6a17bd9a64f9ee046abada5ebffb1ef2ef92ebf3
ckroening <charlottekroening@gmail.com>
2015-11-30 21:09:42 -0600

    got images to render.

--- routes/memes.js
+++ routes/memes.js
@@ -1,9 +1,9 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var memes = require("../public/memes.json");
+var memes = require('../data/memes.json');
 
-router.get("/", function(req, res, next) {
-  res.render("memes", { memes: memes });
+router.get('/', function(req, res, next) {
+  res.render('memes', { memes: memes });
 } );
 
 module.exports = router;
