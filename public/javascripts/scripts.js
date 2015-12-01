e076caac60c9937df421b50557af2b7821e1575c
ckroening <charlottekroening@gmail.com>
2015-12-01 00:00:58 -0600

    got comments to render in browser.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,15 +1,29 @@
 var source = $("#template").html(); 
-var template = Handlebars.compile(source); 
-var data = { 
-  memes: memes
+var template = Handlebars.compile(source);
+
+var getCommentsForMeme = function(meme, comments) {
+  var result = [];
+  for (var i = 0; i < comments.length; i++) {
+    if (comments[i].imageId === meme.imageId) {
+      result.push(comments[i]);
+    }
+  }
+  return result;
 };
-$('body').append(template(data));
+
+var getMemeInfo = function(memes, comments) {
+  for (var i = 0; i < memes.length; i++) {
+    memes[i].comments = getCommentsForMeme(memes[i], comments)
+  }
+  return memes;
+};
 
 $.ajax({
-       url: 'data/comments.jade'
-   })
-   .done(function(json) {
-       console.log(json)
-       var object = JSON.parse(json);
-       console.log(object);
-   });
+    url: '/comments'
+  })
+  .done(function(comments) {
+    var data = { 
+      memeInfo: getMemeInfo(memes, comments)
+    };
+    $('body').append(template(data));
+  });
